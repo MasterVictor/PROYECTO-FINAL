ce9674bf1021d7760c15564440f7408bfd7f07c7
MasterVictor <v-mas10@hotmail.com>
2017-05-25 20:18:49 -0600

    Add files via upload

---
+++ Local.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFor
+{
+    class Local
+    {
+        string nombrecomercial;
+        string actividad;
+    }
+}
